d0f4a0145e2872fe26c7704e213276beaa8d7c2b
Anastaisa <anastas.tarasenko001@mymdc.net>
2025-02-11 14:10:12 -0500

    remove stupid comments

--- html/db.php
+++ html/db.php
@@ -1,15 +1,13 @@
 <?php
 
-// Get database credentials from environment variables
-$DB_HOST = 'db'; // Docker service name for the database (ensure the container is named 'db' in docker-compose.yml)
+$DB_HOST = 'db'; // Docker service name for db
 $DB_USER = getenv('MYSQL_USER');
 $DB_PASS = getenv('MYSQL_PASSWORD');
 $DB_NAME = getenv('MYSQL_DATABASE');
 
-// Establish a connection to the database
 $db = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
-// Check the connection
+// Check connection
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
 }
